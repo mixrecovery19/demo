f1d67c76989f5101c4c54e7b0a5c44bd127158c6
Michael Kalaf <mixrecovery@outlook.com>
2026-06-06 05:06:36 +1000

    Update

--- src/main/resources/static/js/validateArticleForm.js
+++ src/main/resources/static/js/validateArticleForm.js
@@ -1,7 +1,7 @@
 // Client side Javascript validation for the article form.
 // this can also be done with server side validation as per previous examples in the Controllers
 // but this provides a better user experience by providing immediate feedback without the need for a page refresh.
-// which I did not realize that is why we were doing this for but makes perfect sense now.
+// which I did not realize that is why we were doing this for I think the tutor was a bit confused but makes perfect sense now.
 function validateArticleForm() {
 
     const title = document.getElementById("title").value.trim();
